5c9f6825b2a25c61e84211681aa9f283af6cdc02
Lane Rassmussen <42427359+lanerass@users.noreply.github.com>
2018-10-11 22:57:10 -0400

    Added multiple calculations

--- app/src/main/java/com/example/lane/research/MainActivity.java
+++ app/src/main/java/com/example/lane/research/MainActivity.java
@@ -16,22 +16,73 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button calc= (Button) findViewById(R.id.calc);
-        calc.setOnClickListener(new OnClickListener() {//when user clicks calulate button
-            @Override
-            public void onClick(View view) {
-                EditText num1 = (EditText) findViewById(R.id.num1);//finds each number from xml
-                EditText num2 = (EditText) findViewById(R.id.num2);
-                TextView result= (TextView) findViewById(R.id.result);
+        Button add = (Button) findViewById(R.id.add);
+        Button sub = (Button) findViewById(R.id.sub);
+        Button mult = (Button) findViewById(R.id.mult);
+        Button div = (Button) findViewById(R.id.div);
+        add.setOnClickListener(new OnClickListener() {//when user clicks calulate button
+                                   @Override
+                                   public void onClick(View view) {
+                                       EditText num1 = (EditText) findViewById(R.id.num1);//finds each number from xml
+                                       EditText num2 = (EditText) findViewById(R.id.num2);
+                                       TextView result = (TextView) findViewById(R.id.result);
 
-                int number1= Integer.parseInt(num1.getText().toString());//converts values inputted to integers
-                int number2= Integer.parseInt(num2.getText().toString());
-                int resultvalue= number1*number2;
-                result.setText(resultvalue+ "");//returns result of two numbers added
+                                       int number1 = Integer.parseInt(num1.getText().toString());//converts values inputted to integers
+                                       int number2 = Integer.parseInt(num2.getText().toString());
+                                       int resultvalue = number1 + number2;
+                                       result.setText(resultvalue + "");//returns result of two numbers added
 
-            }
+                                   }
 
-    }
-    );
+                               }
+        );
+        sub.setOnClickListener(new OnClickListener() {
+                                   @Override
+                                   public void onClick(View view) {
+                                       EditText num1 = (EditText) findViewById(R.id.num1);
+                                       EditText num2 = (EditText) findViewById(R.id.num2);
+                                       TextView result = (TextView) findViewById(R.id.result);
+
+                                       int number1 = Integer.parseInt(num1.getText().toString());
+                                       int number2 = Integer.parseInt(num2.getText().toString());
+                                       int resultvalue = number1 - number2;
+                                       result.setText(resultvalue + "");
+
+                                   }
+
+                               }
+        );
+        mult.setOnClickListener(new OnClickListener() {//when user clicks calulate button
+                                    @Override
+                                    public void onClick(View view) {
+                                        EditText num1 = (EditText) findViewById(R.id.num1);
+                                        EditText num2 = (EditText) findViewById(R.id.num2);
+                                        TextView result = (TextView) findViewById(R.id.result);
+
+                                        int number1 = Integer.parseInt(num1.getText().toString());
+                                        int number2 = Integer.parseInt(num2.getText().toString());
+                                        int resultvalue = number1 * number2;
+                                        result.setText(resultvalue + "");
+
+                                    }
+
+                                }
+        );
+        div.setOnClickListener(new OnClickListener() {
+                                   @Override
+                                   public void onClick(View view) {
+                                       EditText num1 = (EditText) findViewById(R.id.num1);
+                                       EditText num2 = (EditText) findViewById(R.id.num2);
+                                       TextView result = (TextView) findViewById(R.id.result);
+
+                                       int number1 = Integer.parseInt(num1.getText().toString());
+                                       int number2 = Integer.parseInt(num2.getText().toString());
+                                       int resultvalue = number1 / number2;
+                                       result.setText(resultvalue + "");
+
+                                   }
+
+                               }
+        );
     }
 }
