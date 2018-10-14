d2d1cf75fe18b5af6933fdad6f746f74cb693016
Lane Rassmussen <42427359+lanerass@users.noreply.github.com>
2018-10-14 01:53:31 -0400

    added spinner

--- app/src/main/java/com/example/lane/research/MainActivity.java
+++ app/src/main/java/com/example/lane/research/MainActivity.java
@@ -3,18 +3,28 @@ package com.example.lane.research;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import static android.view.View.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.numbers, android.R.layout.simple_spinner_item);
+       adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       spinner.setAdapter(adapter);
+
 
         Button add = (Button) findViewById(R.id.add);
         Button sub = (Button) findViewById(R.id.sub);
@@ -85,4 +95,5 @@ public class MainActivity extends AppCompatActivity {
                                }
         );
     }
+
 }
